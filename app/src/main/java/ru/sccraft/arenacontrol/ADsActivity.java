7553909896de379d2becb9c407c3dbed82d1b25d
alexandr <sasha01945@gmail.com>
2018-03-12 13:51:11 +0300

    ArenaControl

--- app/src/main/java/ru/sccraft/arenacontrol/ADsActivity.java
+++ app/src/main/java/ru/sccraft/arenacontrol/ADsActivity.java
@@ -1,25 +1,12 @@
 package ru.sccraft.arenacontrol;
 
-import android.accounts.Account;
-import android.accounts.AccountManager;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * Created by alexandr on 16.08.17.
