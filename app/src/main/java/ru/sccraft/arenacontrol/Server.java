414a5924ed7cbfdf1df78511fc6f91298994b658
alexandr <chivarzin@wmid.com>
2018-07-24 13:00:32 +0300

    ArenaControl

--- app/src/main/java/ru/sccraft/arenacontrol/Server.java
+++ app/src/main/java/ru/sccraft/arenacontrol/Server.java
@@ -182,14 +182,14 @@ public class Server {
      */
     private class API_info extends API_ответ {
         byte online; // 0=сервер offline, 1=сервер online, 2=сервер запускается или завис
-        String server_id;
-        String server_name;
-        String server_address;
-        String server_maxslots;
-        String server_location;
-        String server_type;
-        String server_dateblock;
-        String server_daystoblock;
+        String server_id; //ID сервера на хостинге
+        String server_name; //Название сервера
+        String server_address; //IP и порт сервера
+        String server_maxslots; //Максимальное возможное количество подключённых игроков
+        String server_location; //Расположение сервера
+        String server_type; //Тип сервера (Обычно публичный)
+        String server_dateblock; //Сервер оплачен до...
+        String server_daystoblock; //Дней до окончания аренды
         Data data;
 
 
@@ -221,8 +221,8 @@ public class Server {
             class E {
                 String gametype;
                 String game_id;
-                String version;
-                String plugins;
+                String version; //Версия сервера
+                String plugins; //Список плагинов
                 String map;
                 String hostport;
                 String hostip;
