f5aedea170e75aed52e5f0ef16cd50cd7fa80541
alexandr <admin@sccraft.ru>
2019-07-01 19:46:15 +0300

    Update new API Java classes Added comments

--- app/src/main/java/myarena/api/API_console.java
+++ app/src/main/java/myarena/api/API_console.java
@@ -1,5 +1,8 @@
 package myarena.api;
 
+/**
+ * Ответ MyArena.ru API на запрос о консоли сервера (Неофициальный метод - getconsole)
+ */
 public class API_console extends  API_ответ {
 
     String console_log;
