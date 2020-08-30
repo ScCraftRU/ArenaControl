678682130cc3f0bb757d36e221bf4c7e17f86d43
Alexandr <admin@sccraft.ru>
2020-08-30 19:22:23 +0300

    Remove AdView from ADsActivity.kt

--- app/src/main/java/ru/sccraft/arenacontrol/ADsActivity.kt
+++ app/src/main/java/ru/sccraft/arenacontrol/ADsActivity.kt
@@ -2,24 +2,17 @@ package ru.sccraft.arenacontrol
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.ads.AdView
 
 /**
  * Created by alexandr on 16.08.17.
- * Activity с рекламным баннером AdMob
+ * Бывший Activity с рекламным баннеером. Сейчас не отвечает за рекламу
  */
 
 @SuppressLint("Registered")
 open class ADsActivity : AppCompatActivity() {
-    private lateinit var adView: AdView
     protected lateinit var fe: Fe
 
-    protected fun задать_баннер(баннер: AdView) {
-        this.adView = баннер
-        adView.visibility = View.GONE
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
